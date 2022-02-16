d1402780fba906437fe440d033d149406322ba73
willy <willy8742891@gmail.com>
2022-02-16 11:18:54 +0800

    fix: FileName.

--- src/file-name.ts
+++ src/file-name.ts
@@ -4,39 +4,43 @@ export class FileName {
   readonly name: string
 
   constructor(name: string) {
-    const last = name.lastIndexOf('.') 
+    const last = name.lastIndexOf('.')
     this.ext = last >= 0 ? name.substring(last) : ''
     this.name = name.replace(this.ext, '')
-    let index = 0
-    let isBlack = false
-    for (let i = 0; i < this.name.length; i++) {
+    let index = this.data.push(this.name[0])
+    for (let i = 1; i < this.name.length; i++) {
       const str = this.name[i]
-      if (i === 0) {
-        index = this.data.push(str)
-      } else if (/\.|-|_|\s/.test(str)) {
-        isBlack = true
-      } else if (isBlack) {
-        isBlack = false
-        index = this.data.push(str) 
-      } else if (/[A-Z]/.test(str)) {
-        index = this.data.push(str)
-      } else {
-        this.data[index - 1] += str
+      switch (true) {
+        case /\.|-|_|\s/.test(str):
+          i++
+          index = this.data.push(this.name[i])
+          break
+        case /[A-Z]/.test(str):
+          index = this.data.push(str)
+          break
+        default:
+          this.data[index - 1] += str
       }
     }
   }
 
   transformUpperHump(): string {
-    return this.data.filter(s => s).map((s) => s[0].toUpperCase() + s.substring(1)).join('')
+    return this.data
+      .filter((s) => s)
+      .map((s) => s[0].toUpperCase() + s.substring(1))
+      .join('')
   }
 
   transformLowerHump(): string {
-    return this.data.filter(s => s).map((s, i) => {
-      if (i === 0) {
-       return s[0].toLowerCase() + s.substring(1) 
-      }
-      return s[0].toUpperCase() + s.substring(1)
-    }).join('')
+    return this.data
+      .filter((s) => s)
+      .map((s, i) => {
+        if (i === 0) {
+          return s[0].toLowerCase() + s.substring(1)
+        }
+        return s[0].toUpperCase() + s.substring(1)
+      })
+      .join('')
   }
 
   transformKebabCase(): string {
--- tests/file-name.test.ts
+++ tests/file-name.test.ts
@@ -1,5 +1,10 @@
 import { FileName } from '../src/file-name'
 
+// function log(v: unknown) {
+//   console.log(v)
+//   return v
+// }
+
 test('new FileName():', async () => {
   const fileName = new FileName('ssAdd-See_bb')
   expect(fileName.transformUpperHump()).toBe('SsAddSeeBb')
